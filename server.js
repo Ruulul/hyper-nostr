995d211a004e0aa188e5cad060e134cc81edbb1e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-15 13:45:47 -0400

    ....

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ import * as SDK from 'hyper-sdk'
 import goodbye from './goodbye.js'
 import { validateEvent, getEventType } from './nostr_events.js'
 import { default as fsWithCallbacks } from 'fs'
+
+import child_process from 'child_process'
 const fs = fsWithCallbacks.promises
-//const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
 const port = process.argv[2] || 3000
 const startingTopics = process.argv.slice(3)
@@ -18,9 +20,9 @@ const sdk = await SDK.create({
   autoJoin: true
 })
 
+const content = '#nostr #swarm 🤙'
 async function writekey () {
   try {
-    // const content = 'Some content!'
     await fs.appendFile('KEYS', sdk.publicKey.toString('hex'))
     console.log('your key is', sdk.publicKey.toString('hex'))
   } catch (err) {
@@ -28,36 +30,46 @@ async function writekey () {
   }
 }
 writekey()
+const exec = child_process.exec
 
-import child_process from 'child_process';
-const exec = child_process.exec;
+const nostril = ''
 
-const nostril = ""
+exec("type -P nostril || echo ''", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`)
+    return nil
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`)
+    return nil
+  }
+  // console.log(`stdout: ${stdout}`);
+})
 
-exec("which nostril", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return nil;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return nil;
-    }
-    const nostril = stdout;
-});
+exec("which nostril || echo 'nostril not found!!! && 1'", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`)
+    return nil
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`)
+    return nil
+  }
+  const nostril = stdout
+})
 
-exec(`nostril --sec ${sdk.publicKey.toString('hex')} --envelope --content "" `, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`51: error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`55: stderr: ${stderr}`);
-        return;
-    }
-    console.log(`nostril:58: stdout: ${stdout}`);
-    //console.log(`${stdout}`);
-});
+exec(`nostril --sec ${sdk.publicKey.toString('hex')} -t "hyper-swarm" --envelope --content "${content}" `, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`51: error: ${error.message}`)
+    return
+  }
+  if (stderr) {
+    console.log(`55: stderr: ${stderr}`)
+    return
+  }
+  console.log(`nostril:58: stdout: ${stdout}`)
+  // console.log(`${stdout}`);
+})
 
 goodbye(async _ => {
   console.log('exiting...')
