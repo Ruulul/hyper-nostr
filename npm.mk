b41e271d8422d43a057607f2609afcbe45a896aa
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-03 14:17:32 -0400

    npm.mk:package.json:make install gnostr-lfs

--- npm.mk
+++ npm.mk
@@ -1,11 +1,14 @@
+install:npm-install
 npm-install:## 	
 	@npm install
-	
+test:npm-test
 npm-test:## 	
 	@npm run lint && tape tests/**/*.js
+start:npm-start
 npm-start:## 	
 	#@node server.js
 	@npm run start
 # available via `npm run-script`:
+lint:npm-lint
 npm-lint:## 	
 	@npm run lint
